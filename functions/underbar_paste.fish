c6acef26615d81d1df744c3e5a9ca87914910240
nj <4769034+nianjie@users.noreply.github.com>
2023-03-11 11:34:15 +0900

    add a new feature which allow to select items saved in $underbar.

---
+++ functions/underbar_paste.fish
@@ -0,0 +1,32 @@
+function underbar_paste --description 'paste selected item to command line from a menu generated from items contained in $underbar'
+  if count $underbar &>/dev/null
+    list_underbar
+  else
+    printf \a
+  end
+end
+
+function list_underbar --no-scope-shadowing
+  set items_c (count $underbar)
+  set -l letters a b c d e f g h i j k l m n o p q r s t u v w x y z
+  for i in (seq $items_c -1 1)
+    set -l item $underbar[$i]
+    set -l label_color normal
+    set -q fish_color_cwd
+    and set label_color $fish_color_cwd
+    set -l dir_color_reset (set_color normal)
+    set -l dir_color
+
+    printf '%s %s %2d) %s %s%s%s\n' (set_color $label_color) $letters[$i] $i (set_color normal) $dir_color $item $dir_color_reset
+  end
+
+  set -l msg (_ 'Select item by letter or number: ')
+  read -l -p "echo '$msg'" choice
+  if test -z "$choice"
+      return 0
+  else if string match -q -r '^[a-z]$' $choice
+      # Convert the letter to an index number.
+      set choice (contains -i $choice $letters)
+  end
+  commandline -i -- $underbar[$choice]
+end
