cef74596facc12ee61abad3addc8fceb8c3969b6
nj <4769034+nianjie@users.noreply.github.com>
2023-03-12 10:09:21 +0900

    refactor variable name.

--- key_bindings.fish
+++ key_bindings.fish
@@ -3,16 +3,15 @@ set -l funcs underbar underbar_show underbar_paste
 # specific function with its key sequence, one without key assignment not to bind
 set -l underbar \eg
 set -l underbar_paste \ei
-set -l occupied
+set -l conflicted
 for func in $funcs
     set -q $func
     and if not bind -s $$func &>/dev/null
       bind $$func $func
     else
-      set -a occupied (printf 'key-sequence %s is bound to %s.' (bind $$func | string split ' ' --)[-2..-1])
+      set -a conflicted (printf 'key-sequence %s is bound to %s.' (bind $$func | string split ' ' --)[-2..-1])
     end
-    breakpoint
 end
-for occupier in $occupied
-  	printf '[omf-underbar]\aOops, %s\n' $occupier
+for conflict in $conflicted
+  	printf '[omf-underbar]\aOops, %s\n' $conflict
 end
