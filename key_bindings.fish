4cbb98c251d63c6d4ad33b85723b85a5716e1ef3
nj <4769034+nianjie@users.noreply.github.com>
2023-03-12 10:05:19 +0900

    improve key-bind check.

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,13 +1,17 @@
-set -f occupied
-if not bind -s \eg &>/dev/null
-   bind \eg underbar
-else
-   set -a occupied (printf 'key-sequence %s is bound to %s.' (bind \eg | string split ' ' --)[-2..-1])
-end
-if not bind -s \ei &>/dev/null
-   bind \ei underbar_show
-else
-  set -a occupied (printf 'key-sequence %s is bound to %s.' (bind \ei | string split ' ' --)[-2..-1])
+# functions targeted to bind
+set -l funcs underbar underbar_show underbar_paste
+# specific function with its key sequence, one without key assignment not to bind
+set -l underbar \eg
+set -l underbar_paste \ei
+set -l occupied
+for func in $funcs
+    set -q $func
+    and if not bind -s $$func &>/dev/null
+      bind $$func $func
+    else
+      set -a occupied (printf 'key-sequence %s is bound to %s.' (bind $$func | string split ' ' --)[-2..-1])
+    end
+    breakpoint
 end
 for occupier in $occupied
   	printf '[omf-underbar]\aOops, %s\n' $occupier
